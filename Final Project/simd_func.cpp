f934914d7ff721d3966af004daeb696e45ead9d9
G-gagnon3 <122304660+G-gagnon3@users.noreply.github.com>
2023-04-13 15:05:00 -0400

    Update simd_func.cpp

--- Final Project/simd_func.cpp
+++ Final Project/simd_func.cpp
@@ -1,9 +1,63 @@
 #include <immintrin.h>
 #include <iostream>
 
-void maskHiLo(float* dataIn, float* dataOut, float t_lo, float t_hi, int stride, int length){
+
+__m256 loadStrided(char* Dstart, int stride, int pixels){
+    //char items[32];
+    char* items = (char*) aligned_alloc(32,32*sizeof(char));
+    for(int i =0; i<32; i++){
+        if(i<pixels){
+            items[i] = Dstart[stride*i];
+        }else{
+            items[i] = 0;
+        }
+    }
+    return _mm256_load_si256((__m256i*) items);
+}
+
+void storeStrided(char* Dstart, int stride, int pixels){
+    
+}
+
+
+void maskHiLo(char* dataIn, char* dataOut, char t_lo, char t_hi, int stride, int pixels){
     //Process:
     //Create hi and lo comparison vectors
+    __m256i c_lo;
+    __m256i c_hi;
+    c_lo = _mm256_set1_epi8(t_lo);
+    c_hi = _mm256_set1_epi8(t_hi);
+
+    //BEGIN LOOP
+    //int vecPerLoop = 4;
+    __m256i cur_vec;
+    __m256i isUnderHi;
+    __m256i isAboveLo;
+    __m256i inRange;
+
+    long memOffset;
+
+    for(int i = 0; i*32 < pixels; i++){
+        //Load [based on stride] several vectors
+        memOffset = i*32*stride;
+        cur_vec = loadStrided()
+        //Compare and make mask for each loaded
+        isUnderHi = _mm256_cmpgt_epi8(c_hi,cur_vec);
+        printf("%X\n",_mm256_movemask_epi8(isUnderHi));
+        isAboveLo = _mm256_cmpgt_epi8(cur_vec,c_lo);
+        printf("%X\n",_mm256_movemask_epi8(isAboveLo));
+        inRange = _mm256_and_si256(isUnderHi, isAboveLo);
+        printf("%X\n",_mm256_movemask_epi8(inRange));
+        //Mask the vector so everything in range remains
+        cur_vec = _mm256_and_si256(cur_vec, inRange);
+        //Store entire vector to the specific memory location
+        _mm256_store_si256((__m256i*) dataOut + memOffset, cur_vec);
+
+    }
+    
+    
+    
+    
     //BEGIN LOOP
     //Load [based on stride] several vectors
     //Compare and make mask for each loaded
@@ -35,7 +89,7 @@ void maskHiLoGscale(char* dataIn, char* dataOut, char t_lo, char t_hi, int lengt
 
     for(int i = 0; i*32 < length; i++){
         //Load [based on stride] several vectors
-        memOffset = i*8;
+        memOffset = i*32;
         cur_vec = _mm256_load_si256((__m256i*) dataIn+memOffset);
         //Compare and make mask for each loaded
         isUnderHi = _mm256_cmpgt_epi8(c_hi,cur_vec);
