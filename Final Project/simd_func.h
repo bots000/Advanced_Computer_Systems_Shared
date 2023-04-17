0f4b1f083874b1c5d31952d4a85d8f7fb7d719e0
G-gagnon3 <122304660+G-gagnon3@users.noreply.github.com>
2023-04-17 13:14:09 -0400

    simd segmentation

--- Final Project/simd_func.h
+++ Final Project/simd_func.h
@@ -4,8 +4,8 @@
 
 
 struct datPack{    
-    char* d_in;
-    char* wb;
+    unsigned char* d_in;
+    unsigned char* wb;
 };
 
 void* wb_thread(void* dpack){
@@ -15,7 +15,7 @@ void* wb_thread(void* dpack){
     //pthread_exit();
 }
 
-__m256i loadStrided(char* Dstart, int stride, int pixels){
+__m256i loadStrided(unsigned char* Dstart, int stride, int pixels){
     //char items[32];
     char* items = (char*) aligned_alloc(32,32*sizeof(char));
     for(int i =0; i<32; i++){
@@ -25,20 +25,25 @@ __m256i loadStrided(char* Dstart, int stride, int pixels){
             items[i] = 0;
         }
     }
+    //delete items;
     return _mm256_load_si256((__m256i*) items);
 }
 
-void storeStridedST(char* Dstart, __m256i vec, int stride, int pixels){
-    char* tdataIn = (char*) aligned_alloc(32,32*sizeof(char));
-    _mm256_store_si256((__m256i*) tdataIn, vec);
+void storeStridedST(unsigned char* Dstart, __m256i vec, int stride, int pixels){
+    //char* tdataIn = (char*) aligned_alloc(32,32*sizeof(char));
+    unsigned char tdataIn[32];
+    _mm256_storeu_si256((__m256i*) tdataIn, vec);
     for(int i = 0; i < pixels; i++){
+    //printf("before\n");
         Dstart[i*stride] = tdataIn[i];
+    //printf("after\n");
     }
+    //delete tdataIn;
     return;
 
 }
-void storeStridedMT(char* Dstart, __m256i vec, int stride, int pixels){
-    char* tdataIn = (char*) aligned_alloc(32,32*sizeof(char));
+void storeStridedMT(unsigned char* Dstart, __m256i vec, int stride, int pixels){
+    unsigned char* tdataIn = (unsigned char*) aligned_alloc(32,32*sizeof(char));
     _mm256_store_si256((__m256i*) tdataIn, vec);
     pthread_t thread_array[16];
     datPack datArr[16];
@@ -55,11 +60,12 @@ void storeStridedMT(char* Dstart, __m256i vec, int stride, int pixels){
             pthread_join(thread_array[i%pixels], NULL);
         }
     }
+    delete tdataIn;
     return;
 }
 
 
-void maskHiLo(char* dataIn, char* dataOut, char t_lo, char t_hi, int stride, int pixels){
+void maskHiLo(unsigned char* dataIn, unsigned char* dataOut, char t_lo, char t_hi, int stride, int pixels){
     //Process:
     //Create hi and lo comparison vectors
     __m256i c_lo;
@@ -76,27 +82,35 @@ void maskHiLo(char* dataIn, char* dataOut, char t_lo, char t_hi, int stride, int
 
     long memOffset;
 
+    int cur_pixels = 32;
+    printf("Pixels: %d\n",pixels);
     for(int i = 0; i*32 < pixels; i++){
+        printf("Cur pixel: %d\r",i);
+        if(pixels - i*32 < 32){
+            cur_pixels = pixels - i*32 < 32;
+        }
         //Load [based on stride] several vectors
         memOffset = i*32*stride;
-        cur_vec = loadStrided(dataIn+memOffset,stride,pixels);
+        cur_vec = loadStrided(dataIn+memOffset,stride,cur_pixels);
         //Compare and make mask for each loaded
-        isUnderHi = _mm256_cmpgt_epi8(c_hi,cur_vec);
-        printf("%X\n",_mm256_movemask_epi8(isUnderHi));
-        isAboveLo = _mm256_cmpgt_epi8(cur_vec,c_lo);
-        printf("%X\n",_mm256_movemask_epi8(isAboveLo));
+        //isUnderHi = _mm256_cmpgt_epi8(c_hi,cur_vec);
+        isUnderHi = _mm256_cmpeq_epi8( c_hi, _mm256_max_epu8(c_hi,cur_vec));
+        //printf("%X\n",_mm256_movemask_epi8(isUnderHi));
+        //isAboveLo = _mm256_cmpgt_epi8(cur_vec,c_lo);
+        isAboveLo = _mm256_cmpeq_epi8( cur_vec, _mm256_max_epu8(cur_vec,c_lo));
+        //printf("%X\n",_mm256_movemask_epi8(isAboveLo));
         inRange = _mm256_and_si256(isUnderHi, isAboveLo);
-        printf("%X\n",_mm256_movemask_epi8(inRange));
+        //printf("%X\n",_mm256_movemask_epi8(inRange));
         //Mask the vector so everything in range remains
         cur_vec = _mm256_and_si256(cur_vec, inRange);
         //Store entire vector to the specific memory location
         //_mm256_store_si256((__m256i*) dataOut + memOffset, cur_vec);
-        storeStridedST(dataOut+memOffset, cur_vec, stride, pixels);
+        storeStridedST(dataOut+memOffset, cur_vec, stride, cur_pixels);
 
     }
 }
 
-void maskHiLoGscale(char* dataIn, char* dataOut, char t_lo, char t_hi, int length){
+void maskHiLoGscale(unsigned char* dataIn, unsigned char* dataOut, char t_lo, char t_hi, int length){
     //VecReg Usage:
     //16 available
     //0: lo | 1: hi| 2: cur_vec | 3: isUnderHi
@@ -116,18 +130,21 @@ void maskHiLoGscale(char* dataIn, char* dataOut, char t_lo, char t_hi, int lengt
     __m256i inRange;
 
     long memOffset;
-
+    int cur_pixels = 32;
     for(int i = 0; i*32 < length; i++){
+        if(length - i*32 < 32){
+            cur_pixels = length - i*32 < 32;
+        }
         //Load [based on stride] several vectors
         memOffset = i*32;
         cur_vec = _mm256_load_si256((__m256i*) dataIn+memOffset);
         //Compare and make mask for each loaded
         isUnderHi = _mm256_cmpgt_epi8(c_hi,cur_vec);
-        printf("%X\n",_mm256_movemask_epi8(isUnderHi));
+        //printf("%X\n",_mm256_movemask_epi8(isUnderHi));
         isAboveLo = _mm256_cmpgt_epi8(cur_vec,c_lo);
-        printf("%X\n",_mm256_movemask_epi8(isAboveLo));
+        //printf("%X\n",_mm256_movemask_epi8(isAboveLo));
         inRange = _mm256_and_si256(isUnderHi, isAboveLo);
-        printf("%X\n",_mm256_movemask_epi8(inRange));
+        //printf("%X\n",_mm256_movemask_epi8(inRange));
         //Mask the vector so everything in range remains
         cur_vec = _mm256_and_si256(cur_vec, inRange);
         //Store entire vector to the specific memory location
@@ -138,16 +155,4 @@ void maskHiLoGscale(char* dataIn, char* dataOut, char t_lo, char t_hi, int lengt
     
     
     
-}
-
-int main(){
-    char testvector[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    char* tdataIn = (char*) aligned_alloc(32,32*sizeof(char));
-    char* tdataOut = (char*) aligned_alloc(32,32*sizeof(char));
-    for(int i = 0; i< 32; i++){
-        tdataIn[i] = i;
-    }
-    maskHiLo(tdataIn,tdataOut,4,10,1,32);
-    std::cout << tdataOut[6] << std::endl;
-    return 0;
 }
---
+++ Final Project/simd_std_greyscale_SIMD_Filter.cpp
@@ -0,0 +1,161 @@
+#include <stdint.h>
+#include <immintrin.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "simd_func.h"
+
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+
+// first parameter: r5_b4_g4_r4_b3_g3_r3_b2_g2_r2_b1_g1_r1_b0_g0_r0
+// second parameter: b7_g7_r7_b6_g6_r6_b5_g5
+// last three: r,g,b passed by reference
+void GatherRGBx8(const __m128i elements16, const __m128i elements8, __m128i &red, __m128i &green, __m128i &blue){
+    //Shuffle mask for gathering 4 R elements, 4 G elements and 4 B elements (also set last 4 elements to duplication of first 4 elements).
+    const __m128i shuffle_mask = _mm_set_epi8(9,6,3,0, 11,8,5,2, 10,7,4,1, 9,6,3,0);
+
+    __m128i four_through_seven = _mm_alignr_epi8(elements8, elements16, 12);
+
+    //Gather 4 R elements, 4 G elements and 4 B elements.
+    //Remark: As I recall _mm_shuffle_epi8 instruction is not so efficient (I think execution is about 5 times longer than other shuffle instructions).
+    __m128i zero_through_three_red_repeat = _mm_shuffle_epi8(elements16, shuffle_mask);
+    __m128i four_through_seven_red_repeat = _mm_shuffle_epi8(four_through_seven, shuffle_mask);
+
+    //Put 8 R elements in lower part.
+    __m128i lower_red = _mm_alignr_epi8(four_through_seven_red_repeat, zero_through_three_red_repeat, 12);
+
+    //Put 8 G elements in lower part.
+    __m128i green_0_to_3_hi = _mm_slli_si128(zero_through_three_red_repeat, 8);
+    __m128i green_4_to_7_lo = _mm_srli_si128(four_through_seven_red_repeat, 4);
+    __m128i lower_green = _mm_alignr_epi8(green_4_to_7_lo, green_0_to_3_hi, 12);
+
+    //Put 8 B elements in lower part.
+    __m128i blue_0_to_3_hi = _mm_slli_si128(zero_through_three_red_repeat, 4);
+    __m128i blue_4_to_7_lo = _mm_srli_si128(four_through_seven_red_repeat, 8);
+    __m128i lower_blue = _mm_alignr_epi8(blue_4_to_7_lo, blue_0_to_3_hi, 12);
+
+    //Unpack uint8 elements to uint16 elements.
+    red = _mm_cvtepu8_epi16(lower_red);
+    green = _mm_cvtepu8_epi16(lower_green);
+    blue = _mm_cvtepu8_epi16(lower_blue);
+}
+
+
+//Y = 0.2989*R + 0.5870*G + 0.1140*B 
+__m128i Rgb2Yx8(__m128i red, __m128i green, __m128i blue){
+    //Each coefficient is expanded by 2^15, and rounded to int16 (add 0.5 for rounding).
+    const __m128i r_coef = _mm_set1_epi16((short)(0.2989*32768.0 + 0.5));  //8 coefficients - R scale factor.
+    const __m128i g_coef = _mm_set1_epi16((short)(0.5870*32768.0 + 0.5));  //8 coefficients - G scale factor.
+    const __m128i b_coef = _mm_set1_epi16((short)(0.1140*32768.0 + 0.5));  //8 coefficients - B scale factor.
+
+    //Multiply input elements by 64 for improved accuracy.
+    red = _mm_slli_epi16(red, 6);
+    green = _mm_slli_epi16(green, 6);
+    blue = _mm_slli_epi16(blue, 6);
+
+    //Use the special intrinsic _mm_mulhrs_epi16 that calculates round(r*r_coef/2^15).
+    //Calculate Y = 0.2989*R + 0.5870*G + 0.1140*B (use fixed point computations)
+    __m128i grey = _mm_add_epi16(_mm_add_epi16(_mm_mulhrs_epi16(red, r_coef),_mm_mulhrs_epi16(green, g_coef)),_mm_mulhrs_epi16(blue, b_coef));
+
+    //Divide result by 64.
+    grey = _mm_srli_epi16(grey, 6);
+
+    return grey;
+}
+
+//Convert single row from RGB to Grayscale (use SSE intrinsics).
+//I0 points source row, and J0 points destination row.
+//I0 -> rgbrgbrgbrgbrgbrgb...
+//J0 -> yyyyyy
+static void Rgb2GraySingleRow_useSSE(const unsigned char I0[], const int image_width, unsigned char J0[])
+{
+    int x;      //Index in J0.
+    int srcx;   //Index in I0.
+    __m128i red;
+    __m128i green;
+    __m128i blue;
+
+    srcx = 0;
+
+    //Process 8 pixels per iteration.
+    for (x = 0; x < image_width; x += 8)
+    {
+        //Load 8 elements of each color channel R,G,B from first row.
+        __m128i elements16 = _mm_loadu_si128((__m128i*)&I0[srcx]);     //Unaligned load of 16 uint8 elements
+        __m128i elements8  = _mm_loadu_si128((__m128i*)&I0[srcx+16]);  //Unaligned load of (only) 8 uint8 elements (lower half of XMM register).
+
+        //Separate RGB, and put together R elements, G elements and B elements (together in same XMM register).
+        //Result is also unpacked from uint8 to uint16 elements.
+        GatherRGBx8(elements16, elements8, red, green, blue);
+
+        //Calculate 8 Y elements.
+        __m128i grey = Rgb2Yx8(red,green,blue);
+
+        //Pack uint16 elements to 16 uint8 elements (put result in single XMM register). Only lower 8 uint8 elements are relevant.
+        __m128i new_image_row = _mm_packus_epi16(grey, grey);
+
+        //Store 8 elements of Y in row Y0, and 8 elements of Y in row Y1.
+        _mm_storel_epi64((__m128i*)&J0[x], new_image_row);
+
+        srcx += 24; //Advance 24 source bytes per iteration.
+    }
+}
+
+
+void Rgb2Gray_useSSE(unsigned char I[], int image_width,  int image_height, unsigned char J[]){
+    //I0 points source image row.
+    unsigned char *I0;  //I0 -> rgbrgbrgbrgbrgbrgb...
+
+    //J0 points destination image row.
+    unsigned char *J0;  //J0 -> YYYYYY
+
+    int y;  //Row index
+
+    //Process one row per iteration.
+    for (y = 0; y < image_height; y ++)
+    {
+        I0 = &I[y*image_width*3];       //Input row width is image_width*3 bytes (each pixel is R,G,B).
+
+        J0 = &J[y*image_width];         //Output Y row width is image_width bytes (one Y element per pixel).
+
+        //Convert row I0 from RGB to Grayscale.
+        Rgb2GraySingleRow_useSSE(I0, image_width, J0);
+    }
+
+}
+
+int main() {
+    
+    int width, height, bpp;
+
+    unsigned char* rgb_image = stbi_load("example_png_copy.png", &width, &height, &bpp, 3);
+    unsigned char* grey_img = (unsigned char*) aligned_alloc(width * height,width * height*sizeof(unsigned char));
+    unsigned char* filt_img = (unsigned char*) aligned_alloc(32,3*width * height*sizeof(unsigned char));
+
+    auto start_initial_run = high_resolution_clock::now();
+
+    Rgb2Gray_useSSE(rgb_image, width, height, grey_img);
+
+    auto stop_initial_run = high_resolution_clock::now();
+    auto duration_initial_run = duration_cast<microseconds>(stop_initial_run - start_initial_run);
+
+    cout << "Time of initial run (us): " << duration_initial_run.count() << endl;
+
+    maskHiLo(rgb_image, filt_img, 255, 255, 3, width*height);
+    maskHiLo(rgb_image+1, filt_img+1, 0, 255, 3, width*height);
+    maskHiLo(rgb_image+2, filt_img+2, 0, 255, 3, width*height);
+
+    stbi_write_png("try_filter_greyscale.png", width, height, 3, filt_img, width);
+
+
+    return 0;
+}
